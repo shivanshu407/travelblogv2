0dfca3d899776797d0c458f2216078db8a6b4dd0
shivanshu407 <120252478+shivanshu407@users.noreply.github.com>
2025-03-23 19:21:53 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['fv1iqxumfd1pfiq9.public.blob.vercel-storage.com'],
+    },
+};
+
+export default nextConfig;
